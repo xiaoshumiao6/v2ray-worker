21a4535332e40ec9053d349a09ea6c5309eb73ff
xiaoshumiao6 <34533475+xiaoshumiao6@users.noreply.github.com>
2024-10-31 19:29:28 +0800

    Update worker-configuration.d.ts

--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -1,6 +1,7 @@
 interface Env {
 	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
 	// MY_KV_NAMESPACE: KVNamespace;
+	MY_KV_NAMESPACE: v2ray;
 	//
 	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
 	// MY_DURABLE_OBJECT: DurableObjectNamespace;
